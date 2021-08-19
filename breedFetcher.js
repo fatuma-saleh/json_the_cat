1ed1d0037bca83d618b496da3c0870620759638b
Fatuma <sfatex@gmail.com>
2021-08-19 01:11:31 -0400

    used callback in breedFetcher and added test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
-const catBreedArr = process.argv.slice(2);
 const request = require('request');
-request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreedArr[0]}`, (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  if (error) {
-    throw error;
-  }
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const data = JSON.parse(body);
-  //console.log(data);
-  if (data.length === 0) {
-    console.log("Invalid Breed");
-  } else if (data.length > 0) {
-    console.log(typeof data);
-    console.log('Description: ', data[0].description);
-  }
 
-});
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (error) {
+      callback(error, null);
+    }
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description);
+    } else {
+      callback("Breed not found");
+
+    }
+
+  });
+};
+module.exports = { fetchBreedDescription };
